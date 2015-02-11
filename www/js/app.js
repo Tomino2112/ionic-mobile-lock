eb0e88005933c0a8cdc11a8d01d29d1d33befcbe
Tomino2112 <dostal.it@gmail.com>
2015-02-11 01:35:24 +0000

    Adding the lines while dragging - super neat

--- www/js/app.js
+++ www/js/app.js
@@ -137,8 +137,24 @@ angular.module('starter').controller('ScreenLockCtrl',['$scope','$timeout','$int
 
       if ($scope.block) return;
 
+      // Line
+      if ($scope.currentLine){
+          var line = $scope.currentLine;
+          var startPoint = [parseInt(line.style.left),parseInt(line.style.top)],
+              endPoint = [e.gesture.center.pageX,e.gesture.center.pageY],
+              length = Math.sqrt((endPoint[0] - startPoint[0]) * (endPoint[0] - startPoint[0])
+              + (endPoint[1] - startPoint[1]) * (endPoint[1] - startPoint[1])),
+              angle = 180 / 3.1415 * Math.acos((endPoint[1] - startPoint[1]) / length);
+
+          if(endPoint[0] > startPoint[0])
+              angle *= -1;
+
+          line.style.height=(length-1)+"px";
+          line.style.transform= "rotate("+angle+"deg)";
+      }
+
     // Bubble up
-    var noddy = e.target; console.log(e);
+    var noddy = document.elementFromPoint(e.gesture.center.pageX,e.gesture.center.pageY);
 
     while(!classy.hasClass(noddy,'point') && noddy) {
       noddy = noddy.parentNode;
@@ -155,7 +171,14 @@ angular.module('starter').controller('ScreenLockCtrl',['$scope','$timeout','$int
 
       if ($scope.block) return;
 
+      if ($scope.currentLine && $scope.currentLine.parentNode){
+          $scope.currentLine.parentNode.removeChild($scope.currentLine);
+      }
+
     if ($scope.currentComb.length) {
+        var combination = document.getElementsByClassName('screenlock')[0];
+        classy.addClass(combination,'finished');
+
       if ($scope.currentComb.join('') === $scope.correctCombination) {
         $scope.$apply(function(){
           $scope.result = 'Thats right!';
@@ -178,6 +201,9 @@ angular.module('starter').controller('ScreenLockCtrl',['$scope','$timeout','$int
     },800);
   },angular.element(document.getElementsByClassName('combination')[0]));
 
+    $scope.currentLine = null;
+    $scope.lastPoint = null;
+
     function highlighPoint(el, first){
         if (typeof first === 'undefined'){
             first = false;
@@ -195,13 +221,78 @@ angular.module('starter').controller('ScreenLockCtrl',['$scope','$timeout','$int
                 }
             }
 
+            if ($scope.currentLine && $scope.currentComb.length>0){
+                endLine($scope.lastPoint, el, $scope);
+            }
+            addLine(el,$scope);
+
+            $scope.lastPoint = el;
+
             classy.addClass(el,'active');
             $scope.currentComb.push(pointId);
             if (ionic.Platform.isWebView()) {
-                $cordovaVibration.vibrate(30);
+                $cordovaVibration.vibrate(10);
             }
         }
     }
+
+    function addLine(point,$scope){
+        // Create line
+        var line = document.createElement('div'),
+            span = point.getElementsByTagName('span')[0],
+            offset = getOffset(span),
+            spanWidth = parseInt(span.offsetWidth);
+
+        classy.addClass(line,'line');
+        line.style.top = (offset.top+(spanWidth/2))+'px';
+        line.style.left = (offset.left+(spanWidth/2))+'px';
+
+        var root = document.getElementsByClassName('screenlock')[0];
+        root.appendChild(line);
+
+        $scope.currentLine = line;
+    }
+
+    function endLine(last, current, $scope){
+        var span = current.getElementsByTagName('span')[0],
+            spanWidth = span.offsetWidth,
+            offset = getOffset(current.getElementsByTagName('span')[0]),
+            startPoint = [parseInt($scope.currentLine.style.left),parseInt($scope.currentLine.style.top)],
+            endPoint = [(offset.left+(spanWidth/3)),(offset.top+(spanWidth/3))],
+            length = Math.sqrt((endPoint[0] - startPoint[0]) * (endPoint[0] - startPoint[0])
+            + (endPoint[1] - startPoint[1]) * (endPoint[1] - startPoint[1]))+(spanWidth/3),
+            angle = 180 / 3.1415 * Math.acos((endPoint[1] - startPoint[1]) / length);
+
+        if (angle>0 && angle<35){
+            angle = 0;
+        } else if (angle>34 && angle<80) {
+            angle = 45;
+        } else if (angle>79 && angle<125){
+            angle = 90;
+        } else if (angle>124 && angle<160){
+            angle = 135;
+        } else {
+            angle = 180;
+        }
+
+        if(endPoint[0] > startPoint[0]) {
+            angle *= -1;
+        }
+
+        $scope.currentLine.style.height=length+"px";
+        $scope.currentLine.style.transform= "rotate("+angle+"deg)";
+
+        $scope.currentLine = null;
+    }
+
+    function getOffset(el) {
+        el = el.getBoundingClientRect();
+        return {
+            left: el.left + window.scrollX,
+            top: el.top + window.scrollY
+        }
+    }
+
 
     function blockScreenlock(){
         var root = document.getElementsByClassName('screenlock')[0];
@@ -234,9 +325,17 @@ angular.module('starter').controller('ScreenLockCtrl',['$scope','$timeout','$int
           $scope.result = '';
       }
 
+      var combination = document.getElementsByClassName('screenlock')[0];
+      classy.removeClass(combination,'finished');
+
     var points = document.getElementsByClassName('point');
     for(var i=0;i<points.length;i++){
       classy.removeClass(points[i],'active');
     }
+
+    var lines = root.getElementsByClassName('line');
+      while(lines.length > 0){
+          lines[0].parentNode.removeChild(lines[0]);
+      }
   }
 }]);
